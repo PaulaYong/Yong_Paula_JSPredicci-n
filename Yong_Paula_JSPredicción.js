9234197a9289ba1f458518c603c91df6dd1530cf
paulayong <pyongzea@gmail.com>
2022-05-08 19:23:27 -0500

    Subiendo JS Predicción

---
+++ Yong_Paula_JSPredicción.js
@@ -0,0 +1,51 @@
+/* Predicción 1: Predicción 1: Qué indicará console.log cuando se llame a esta función?
+
+function myBirthYearFunc(){
+        console.log("Nací en" + 1980);
+    } 
+RESULTADO: Nací en1980
+*/
+
+
+
+/*var birthYearInput = 1980;
+function myBirthYearFunc(birthYearInput){
+        console.log("Nací en" + birthYearInput);
+    }
+
+myBirthYearFunc(birthYearInput)
+
+RESULTADO: Nací en1980
+*/
+
+
+
+//Predicción 3: Intentémoslo otra vez. Si var num1 = 10 y var num2 = 20, ¿qué indicaría console.log?
+
+/* var num1= 10;
+var num2= 20;
+
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 es: " + num1);
+    console.log("num2 es: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+
+add(num1, num2)
+
+RESULTADOS:
+¡Sumando números!
+num1 es: 10
+num2 es: 20
+30 
+*/
+
+
+
+
+
+
+
+
